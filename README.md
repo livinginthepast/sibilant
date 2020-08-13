7a715702d5516aabb8a75eea724ecc954f76c585
Eric Saxby <sax@livinginthepast.org>
2020-08-13 11:01:24 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Sibilant
+========
+
+Static website generator.
+
+## Installation
+
+```elixir
+def deps do
+  [
+    {:sibilant, "~> 0.1"}
+  ]
+end
+```
+
+
---
+++ mix.exs
@@ -0,0 +1,33 @@
+defmodule Sibilant.MixProject do
+  use Mix.Project
+
+  @version "0.1.0"
+
+  def project do
+    [
+      app: :sibilant,
+      deps: deps(),
+      elixir: "~> 1.10",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      version: @version
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:earmark, "~> 1.4"},
+      {:liquid, ">= 1.0.0-pre"},
+      {:yaml_elixir, "~> 2.5"}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+end
